a3b2466264195a92c8d2661c1bddfd9390c01954
Dcaceresvduocuc <D.caceresv@duocuc.cl>
2024-06-15 12:09:18 -0400

    carga cambios
    carga cambios

---
+++ calculadora.py.py
@@ -0,0 +1,29 @@
+num1 = int(input("numero 1: ")) 
+num2 = int(input("numero 2: ")) 
+num3 = int(input("numero 3: "))
+valor = 0
+while True:
+    print("""seleccione opcion
+            1- Sumar 
+            2- Restar
+            3- Multiplicar
+            4- dividir 
+        """)
+
+    valor = int(input("Elige una opcion: ") )     
+
+    if valor == 1:
+        print("la suma es",num1+num2+num3)
+        break;
+    if valor == 2:
+        print("la resta es",num1-num2-num3)
+        break;
+    if valor == 3:
+        print("la multiplicacion es",num1*num2*num3)
+        break;
+    if valor == 4:
+        print("la division es",num1/num2/num3)
+        break;
+    else:
+        print("Opcion incorrecta")
+        break;
